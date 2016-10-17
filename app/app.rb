74714a94d175e242c2185def793cb707b3260a9f
Peter Miklos <pt.miklos@gmail.com>
2016-10-17 12:19:54 +0100

    app.rb updated

--- app/app.rb
+++ app/app.rb
@@ -10,11 +10,14 @@ class App < Sinatra::Base
   end
 
   get "/set" do
-    session[:somekey] = params[:somekey]
+    session[:requests] ||= AppModel.new
+    session[:requests].set_kv_pair(request.query_string)
+    # session[:somekey] = params[:somekey]
   end
 
   get "/get" do
-    session[params[:key].to_sym]
+    session[:requests].get_value(params[:key])
+    # session[params[:key]]
   end
 
   # start the server if ruby file executed directly
